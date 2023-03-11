68df2d6c461eb43e695a978894cabc184f8fa2e1
AGT-Slade <83044137+AGT-Slade@users.noreply.github.com>
2023-03-10 20:47:38 -0600

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("nav ul li:nth-of-type(1)").click(function () {
+  $("nav ul.nav-list li:nth-of-type(1)").click(function () {
     $("nav ul.side-nav-list").toggle(500);
     $(this).toggleClass("toggle-menu");
   });
